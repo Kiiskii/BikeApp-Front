9959209d48436c9ff9cab73755e7bdde2288adb5
Anton <antonoliverkiiski@gmail.com>
2023-04-28 20:07:51 +0300

    Make base for footer component

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent {
-
+  faGithub = faGithub;
 }
