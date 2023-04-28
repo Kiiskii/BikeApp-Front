17cb0e3b1e56e56685e4fdc8449bf3b12fda8c47
Anton <antonoliverkiiski@gmail.com>
2023-04-28 22:36:13 +0300

    Get stations data from backend and make basic structure to show the data in stations component

--- src/app/stations/stations.component.ts
+++ src/app/stations/stations.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-stations',
   templateUrl: './stations.component.html',
-  styleUrls: ['./stations.component.css']
+  styleUrls: ['./stations.component.css'],
 })
 export class StationsComponent {
+  constructor(private apiservice: ApiService) {}
 
+  stations: any = [];
+
+  ngOnInit() {
+    this.apiservice.getStations().subscribe((response) => {
+      console.log(response);
+      this.stations = response;
+    });
+  }
 }
