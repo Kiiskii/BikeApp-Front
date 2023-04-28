ab577ea962d1387834ac6dc0965c3d9a1d69aa53
Anton <112481379+Kiiskii@users.noreply.github.com>
2023-04-28 16:45:36 +0300

    First working version. Feature/setup (#2)
    * Setup connection between frontend and backend
    
    * Print some data in frontend to see if it communicates with backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'BikeApp-Front';
+
+  constructor(private apiservice: ApiService) {}
+
+  journeys: any = [];
+
+  ngOnInit() {
+    this.apiservice.getJourneys().subscribe((response) => {
+      console.log(response);
+      this.journeys = response;
+    });
+  }
 }
