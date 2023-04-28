a5b912856344e0721f4eb2e2e48ace2844f8b18d
Anton <antonoliverkiiski@gmail.com>
2023-04-28 17:37:09 +0300

    Generate home, header, footer, journeys and stations components. Move apiservice and back connection to journeys-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
@@ -8,15 +7,4 @@ import { ApiService } from './api.service';
 })
 export class AppComponent {
   title = 'BikeApp-Front';
-
-  constructor(private apiservice: ApiService) {}
-
-  journeys: any = [];
-
-  ngOnInit() {
-    this.apiservice.getJourneys().subscribe((response) => {
-      console.log(response);
-      this.journeys = response;
-    });
-  }
 }
