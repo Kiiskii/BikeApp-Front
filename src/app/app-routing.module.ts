2aeaacec744ecde0aa1482403b195bb69cb59cf7
Anton <antonoliverkiiski@gmail.com>
2023-05-16 16:25:12 +0300

    Get details data, departure count and return count to the frontend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { JourneysComponent } from './journeys/journeys.component';
 import { StationsComponent } from './stations/stations.component';
+import { DetailsComponent } from './details/details.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'journeys', component: JourneysComponent },
   { path: 'stations', component: StationsComponent },
+  { path: 'details/:fid', component: DetailsComponent },
 ];
 
 @NgModule({
