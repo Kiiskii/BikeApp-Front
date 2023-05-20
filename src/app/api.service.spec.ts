b21f3a0169b1f79b2445dc5b205b3744cdee3142
Anton <112481379+Kiiskii@users.noreply.github.com>
2023-05-20 21:55:04 +0300

    Testing (#18)
    * Remove Capitalization due to backend not caring about it any longer
    
    * Implement test for convertSecondToMinutes function
    
    * Hotfix stations to look like links
    
    * Install Cypress and write first E2E test to see if Journeys sends you to correct site
    
    * E2E test for Stations routerlink
    
    * E2E test for Home routerlink
    
    * E2E test to check if Journeys has a table in it
    
    * E2E test to see if Stations has table in it
    
    * E2E tests to check that station links work and that pagination changes to next page
    
    * E2E test for previous page button
    
    * Add   "include": ["src/**/*.ts"],
    
    * Implement unit tests to see that functions return data from api

--- src/app/api.service.spec.ts
+++ src/app/api.service.spec.ts
@@ -1,17 +1,118 @@
 import { TestBed } from '@angular/core/testing';
-
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { ApiService } from './api.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Journey } from './journey.model';
+import { Station } from './station.model';
+import { Details } from './details.model';
 
 describe('ApiService', () => {
   let service: ApiService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({ imports: [HttpClientTestingModule] });
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ApiService],
+    });
     service = TestBed.inject(ApiService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  it('should retrieve journeys from the API', () => {
+    const mockJourneys: Journey[] = [];
+    const page = 1;
+
+    service.getJourneys(page).subscribe((journeys: Journey[]) => {
+      expect(journeys).toEqual(mockJourneys);
+    });
+
+    const req = httpMock.expectOne(
+      `http://localhost:3000/api/journeys?page=${page}`
+    );
+    expect(req.request.method).toBe('GET');
+    req.flush(mockJourneys);
+  });
+
+  it('should retrieve the number of journey pages from the API', () => {
+    const mockPageCount = 5;
+
+    service.getJourneyPages().subscribe((pageCount: number) => {
+      expect(pageCount).toEqual(mockPageCount);
+    });
+
+    const req = httpMock.expectOne('http://localhost:3000/api/journeycount');
+    expect(req.request.method).toBe('GET');
+    req.flush(mockPageCount);
+  });
+
+  it('should retrieve stations from the API', () => {
+    const mockStations: Station[] = [];
+    const page = 1;
+
+    service.getStations(page).subscribe((stations: Station[]) => {
+      expect(stations).toEqual(mockStations);
+    });
+
+    const req = httpMock.expectOne(
+      `http://localhost:3000/api/stations?page=${page}`
+    );
+    expect(req.request.method).toBe('GET');
+    req.flush(mockStations);
+  });
+
+  it('should retrieve the number of station pages from the API', () => {
+    const mockPageCount = 3;
+
+    service.getStationPages().subscribe((pageCount: number) => {
+      expect(pageCount).toEqual(mockPageCount);
+    });
+
+    const req = httpMock.expectOne('http://localhost:3000/api/stationcount');
+    expect(req.request.method).toBe('GET');
+    req.flush(mockPageCount);
+  });
+
+  it('should retrieve search results from the API', () => {
+    const mockSearchQuery = 'example';
+    const mockSearchResults: Station[] = [];
+    const page = 1;
+
+    service
+      .getSearchResult(mockSearchQuery, page)
+      .subscribe((results: Station[]) => {
+        expect(results).toEqual(mockSearchResults);
+      });
+
+    const req = httpMock.expectOne(
+      `http://localhost:3000/api/search/${mockSearchQuery}?page=${page}`
+    );
+    expect(req.request.method).toBe('GET');
+    req.flush(mockSearchResults);
+  });
+
+  it('should retrieve details from the API', () => {
+    const mockDetailsId = 1;
+    const mockDetails: Details = {
+      stations: [],
+      departurecount: 10,
+      returncount: 5,
+    };
+
+    service.getDetails(mockDetailsId).subscribe((details: Details) => {
+      expect(details).toEqual(mockDetails);
+    });
+
+    const req = httpMock.expectOne(
+      `http://localhost:3000/api/details/${mockDetailsId}`
+    );
+    expect(req.request.method).toBe('GET');
+    req.flush(mockDetails);
   });
 });
